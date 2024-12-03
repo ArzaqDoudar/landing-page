a23bcf5beec9544e1091acd65c0698c030223df9
arzaqziad <arzaqziad20@gmail.com>
2024-12-03 14:33:11 +0200

    feat: build the navbar menu

--- js/app.js
+++ js/app.js
@@ -23,26 +23,80 @@
  * 
 */
 
+const VALUE = 100
+let sectionArray = [];
+
+const sections = document.getElementsByTagName("section")
+const main_hero = document.getElementsByClassName("main__hero")
+
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+*
 */
 
+const getSectionsId = () => {
 
+    for (let index = 0; index < sections.length; index++) {
+        sectionArray.push({ id: sections[index].id, data: sections[index].getAttribute('data-nav') })
 
+    }
+    console.log('sectioArray', sectionArray);
+
+}
+getSectionsId();
+
+const createUlElement = (unorderedList) => {
+    if (unorderedList) {
+
+        sectionArray.forEach(item => {
+            const list_item = document.createElement('li');
+            const list_item_link = document.createElement('a');
+            list_item_link.setAttribute('class', "menu__link");
+            list_item_link.setAttribute('href', `#${item.id}`);
+            list_item_link.innerText = item.data;
+            list_item.appendChild(list_item_link)
+            navbar_list.appendChild(list_item)
+        });
+    }
+}
+
+const makeActive = () => {
+    for (const section of sections) {
+        const section2Top = sections[1].getBoundingClientRect();
+        const box = section.getBoundingClientRect();
+
+        if (box.top <= (box.height) && (box.bottom - VALUE <= box.height) && (box.bottom > VALUE)) {
+            //apply active state on current section and corresponding Nav link
+            addActiveClass(section, 'your-active-class')
+
+        } else {
+            //Remove active state from other section and corresponding Nav link
+            removeActiveClass(section, 'your-active-class')
+        }
+    }
+}
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
 
+const navbar_list = document.getElementById('navbar__list');
+createUlElement(navbar_list);
 
 // Add class 'active' to section when near top of viewport
-
+const addActiveClass = (elem, className) => {
+    const elementClassList = elem.classList;
+    elementClassList.add(className)
+}
+const removeActiveClass = (elem, className) => {
+    const elementClassList = elem.classList;
+    elementClassList.remove(className)
+}
 
 // Scroll to anchor ID using scrollTO event
 
@@ -50,7 +104,7 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
@@ -59,4 +113,4 @@
 
 // Set sections as active
 
-
+document.addEventListener('scroll', () => makeActive())
